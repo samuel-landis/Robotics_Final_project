bbf34fb3292585ff1d39f572d10317d36b938b04
sam111111111 <85041149+sam111111111@users.noreply.github.com>
2022-02-08 14:01:34 -0700

    Update robot_v1.ino

--- code/robot_v1.ino
+++ code/robot_v1.ino
@@ -1,72 +1,49 @@
-#include <SPI.h>     
-#include "RF24.h"            
+#include <SPI.h>      
+#include "RF24.h"     
+#include <Adafruit_PWMServoDriver.h>
 
-int msg[6]; 
+RF24 radio(9,10);     /* 9 and 10 CE and CSN are connected.*/
 
-int VRx1 = A0;
-int VRy1 = A1;
-int VRx2 = A2;
-int VRy2 = A3;
-int VRx3 = A4;
-int VRy3 = A5;
+const uint64_t pipe = robot15772sLandis; 
 
-int xPosition1 = 0;
-int yPosition1 = 0;
-int xPosition2 = 0;
-int yPosition2 = 0;
-int xPosition3 = 0;
-int yPosition3 = 0;
+int msg[6];
 
-int val_0 = 0;
-int val_1 = 0;
-int val_2 = 0;
-int val_3 = 0;
-int val_4 = 0;
-int val_5 = 0;
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-RF24 radio(9,10);                     //9 and 10 are a digital pin numbers to which signals CE and CSN are connected.
-                                      
-const uint64_t pipe = 15772; 
+#define MIN_PULSE_WIDTH 650
+#define MAX_PULSE_WIDTH 2350
+#define DEFAULT_PULSE_WIDTH 1500
+#define FREQUENCY 50
 
-
-void setup(){
-  Serial.begin(9600);
-  Serial.println("Robotics project /n Samuel Landis /n slandis@unm.edu");
-  pinMode(VRx1, INPUT);
-  pinMode(VRy1, INPUT); 
-  pinMode(VRx2, INPUT);
-  pinMode(VRy2, INPUT);
-  pinMode(VRx3, INPUT);
-  pinMode(VRy3, INPUT);
-  radio.begin();             
-  radio.openWritingPipe(pipe); 
+int pulseWidth(int angle){
+int pulse_wide, analog_value;
+pulse_wide = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
+Serial.println(analog_value);
+return analog_value;
 }
 
-void loop(){ 
-  yPosition1 = analogRead(VRy1);
-  Serial.println(yPosition1);
-  val_0 = map(yPosition1, 1023, 0, 170, 10);
-  
-  xPosition1 = analogRead(VRx1);
-  val_1 = map(xPosition1, 1023, 0, 170, 10);
-  
-  yPosition2 = analogRead(VRy2); 
-  val_2 = map(yPosition2, 1023, 0, 170, 10);
-  
-  xPosition2 = analogRead(VRx2);
-  val_3 = map(xPosition2, 1023, 0, 170, 10);
-  
-  yPosition3 = analogRead(VRy3); 
-  val_4 = map(yPosition3, 1023, 0, 170, 10);
-  
-  xPosition3 = analogRead(VRx3);
-  val_5 = map(xPosition3, 1023, 0, 170, 10);
+void setup(){
+Serial.begin(9600);
+Serial.println("Robotics project /n Samuel Landis /n slandis@unm.edu");
+pwm.begin();
+pwm.setPWMFreq(FREQUENCY);  
+radio.begin();                    
+radio.openReadingPipe(1, pipe);  
+radio.startListening();          
+}
 
-  msg[0] = val_0;
-  msg[1] = val_1;
-  msg[2] = val_2;
-  msg[3] = val_3;
-  msg[4] = val_4;
-  msg[5] = val_5;
-  radio.write(msg, sizeof(msg));
+void loop(){
+  if(radio.available()){
+    bool done = false;
+    while (!done){
+    done = radio.read(msg, sizeof(msg));
+    pwm.setPWM(5, 0, pulseWidth(msg[5]));
+    pwm.setPWM(4, 0, pulseWidth(msg[4]));
+    pwm.setPWM(3, 0, pulseWidth(msg[3]));
+    pwm.setPWM(2, 0, pulseWidth(msg[2]));
+    pwm.setPWM(1, 0, pulseWidth(msg[1]));
+    pwm.setPWM(0, 0, pulseWidth(msg[0]));
+    }
+  }
 }
