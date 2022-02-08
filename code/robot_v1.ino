b92b93a08230b431c10b7920c1414e256f337317
sam111111111 <85041149+sam111111111@users.noreply.github.com>
2022-02-08 11:24:39 -0700

    Update robot_v1.ino

--- code/robot_v1.ino
+++ code/robot_v1.ino
@@ -1,8 +1,13 @@
-#include <Wire.h>
-#include <nRF24L01.h>
-#include <SPI.h>
-#include "RF24.h"
+#include <Servo.h>    //the library which helps us to control the servo motor
+#include <SPI.h>      //the communication interface with the modem
+#include "RF24.h"     //the library which helps us to control the radio modem
 #include <Adafruit_PWMServoDriver.h>
+
+RF24 radio(9,10);     /* 9 and 10 CE and CSN are connected.*/
+
+const uint64_t pipe = robot15772sLandis; //the address of the modem,that will receive data from the Arduino.
+
+int msg[6];
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -10,9 +15,6 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define MAX_PULSE_WIDTH 2350
 #define DEFAULT_PULSE_WIDTH 1500
 #define FREQUENCY 50
-
-
-
 
 int pulseWidth(int angle){
 int pulse_wide, analog_value;
@@ -22,20 +24,27 @@ Serial.println(analog_value);
 return analog_value;
 }
 
-
 void setup(){
 Serial.begin(9600);
-Serial.println("16 channel Servo test!");
+Serial.println("Robotics project /n Samuel Landis /n slandis@unm.edu");
 pwm.begin();
-pwm.setPWMFreq(FREQUENCY);
+pwm.setPWMFreq(FREQUENCY);  
+radio.begin();                    //it activates the modem.
+radio.openReadingPipe(1, pipe);   //determines the address of our modem which receive data.
+radio.startListening();           //enable receiving data via modem
 }
 
 void loop(){
-pwm.setPWM(1, 0, pulseWidth(10));//first one is servo num
-delay(1000);
-pwm.setPWM(0, 0, pulseWidth(170));
-delay(1000);
-pwm.setPWM(0, 0, pulseWidth(90));
-delay(1000);
+  if(radio.available()){
+    bool done = false;
+    while (!done){
+    done = radio.read(msg, sizeof(msg));
+    pwm.setPWM(5, 0, pulseWidth(msg[5]));
+    pwm.setPWM(4, 0, pulseWidth(msg[4]));
+    pwm.setPWM(3, 0, pulseWidth(msg[3]));
+    pwm.setPWM(2, 0, pulseWidth(msg[2]));
+    pwm.setPWM(1, 0, pulseWidth(msg[1]));
+    pwm.setPWM(0, 0, pulseWidth(msg[0]));
+    }
+  }
 }
-
